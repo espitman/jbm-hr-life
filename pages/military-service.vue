54dfeb3afef13e8738fd7d0fddad39276f63cee0
saeed heidari <s.heidari@jabama.ir>
2025-04-25 10:56:03 +0330

    Add full-width image to military-service page

--- pages/military-service.vue
+++ pages/military-service.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <PageHeaderCard title="خدمت سربازی" />
+  <div class="container mx-auto p-6" dir="rtl">
+    <PageHeaderCard title="خدمت سربازی" :showDot="false" />
+    
+    <!-- Full width image -->
+    <div class="w-full mt-8">
+      <img src="/public/images/military/1.jpg" alt="خدمت سربازی" class="w-full rounded-lg shadow-lg" />
+    </div>
   </div>
 </template>
 
