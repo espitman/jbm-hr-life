6bb971d005b706dacb00a292892361a268830bef
saeed heidari <s.heidari@jabama.ir>
2025-04-26 01:41:55 +0330

    fix: make form inputs full width on mobile view

--- pages/military-service.vue
+++ pages/military-service.vue
@@ -4,7 +4,7 @@
     
     <!-- Full width image -->
     <div class="w-full">
-      <img src="/images/military/1.jpg" alt="خدمت سربازی" class="w-full" />
+      <img src="~/public/images/military/1.jpg" alt="خدمت سربازی" class="w-full" />
     </div>
     
     <!-- Options Component -->
