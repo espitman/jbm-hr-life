77705768b35eb742db1297201826fafa07b83b38
saeed heidari <s.heidari@jabama.ir>
2025-04-26 01:58:51 +0330

    fix: make all input fields full width in mobile view

--- pages/hr-contact.vue
+++ pages/hr-contact.vue
@@ -5,7 +5,7 @@
       :showDot="false"
     />
     
-    <div class="p-8">
+    <div class="">
       <!-- Contact Cards Grid -->
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <!-- Card 1 - CPO -->
