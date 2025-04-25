855fc3363676ecd31f9c500cc72c2af85b3787f6
saeed heidari <s.heidari@jabama.ir>
2025-04-26 01:48:07 +0330

    fix: show mobile menu button only on mobile screens

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -3,7 +3,7 @@
     <!-- Mobile Menu Button -->
     <button 
       @click="isOpen = !isOpen"
-      class="fixed top-6 left-4 z-[10000] bg-white text-gray-600 rounded-lg p-2"
+      class="fixed top-6 left-4 z-[10000] bg-white text-gray-600 rounded-lg p-2  md:hidden"
     >
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
