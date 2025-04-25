1f05b2054b60c2e33a9f55a923c3413124169f70
saeed heidari <s.heidari@jabama.ir>
2025-04-26 00:18:25 +0330

    update: adjust mobile responsive styles for Section and Story components

--- components/ui/Section.vue
+++ components/ui/Section.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="w-[calc(100vw-16rem)] flex items-center justify-center bg-white">
+  <section class="w-full md:w-[calc(100vw-16rem)] flex items-center justify-center bg-white">
     <slot />
   </section>
 </template> 
