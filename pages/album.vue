19bc349ab01327918e99d7ff39b74dc83290ee12
saeed heidari <s.heidari@jabama.ir>
2025-04-28 19:55:19 +0330

    feat: add lightbox modal to album page with click-to-open and close functionality

--- pages/album.vue
+++ pages/album.vue
@@ -17,48 +17,81 @@
           <img
             :src="image.url"
             :alt="image.title || 'Album photo'"
-            class="w-full h-96 object-cover scale-110 transition-transform duration-300 group-hover:translate-x-4"
+            class="w-full h-96 object-cover scale-110 transition-transform duration-300 group-hover:translate-x-4 cursor-pointer"
+            @click="openLightbox(image)"
           />
         </div>
       </div>
+    </div>
+
+    <!-- Lightbox Modal -->
+    <div
+      v-if="showLightbox"
+      class="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50"
+      @click="closeLightbox"
+    >
+      <img
+        :src="lightboxImage.url"
+        :alt="lightboxImage.title || 'Album photo'"
+        class="max-h-[90vh] max-w-[90vw] object-contain rounded-lg shadow-2xl"
+        @click.stop
+      />
+      <button
+        class="absolute top-8 right-8 text-white text-3xl font-bold"
+        @click="closeLightbox"
+      >×</button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 // Use your real images array here
 const images = [
-  { url: 'https://picsum.photos/id/0/5000/3333' },
-  { url: 'https://picsum.photos/id/1/5000/3333' },
-  { url: 'https://picsum.photos/id/2/5000/3333' },
-  { url: 'https://picsum.photos/id/3/5000/3333' },
-  { url: 'https://picsum.photos/id/4/5000/3333' },
-  { url: 'https://picsum.photos/id/5/5000/3334' },
-  { url: 'https://picsum.photos/id/6/5000/3333' },
-  { url: 'https://picsum.photos/id/7/4728/3168' },
-  { url: 'https://picsum.photos/id/8/5000/3333' },
-  { url: 'https://picsum.photos/id/9/5000/3269' },
-  { url: 'https://picsum.photos/id/10/2500/1667' },
-  { url: 'https://picsum.photos/id/11/2500/1667' },
-  { url: 'https://picsum.photos/id/12/2500/1667' },
-  { url: 'https://picsum.photos/id/13/2500/1667' },
-  { url: 'https://picsum.photos/id/14/2500/1667' },
-  { url: 'https://picsum.photos/id/15/2500/1667' },
-  { url: 'https://picsum.photos/id/16/2500/1667' },
-  { url: 'https://picsum.photos/id/17/2500/1667' },
-  { url: 'https://picsum.photos/id/18/2500/1667' },
-  { url: 'https://picsum.photos/id/19/2500/1667' },
-  { url: 'https://picsum.photos/id/20/3670/2462' },
-  { url: 'https://picsum.photos/id/21/3008/2008' },
-  { url: 'https://picsum.photos/id/22/4434/3729' },
-  { url: 'https://picsum.photos/id/23/3887/4899' },
-  { url: 'https://picsum.photos/id/24/4855/1803' },
-  { url: 'https://picsum.photos/id/25/5000/3333' },
-  { url: 'https://picsum.photos/id/26/4209/2769' },
-  { url: 'https://picsum.photos/id/27/3264/1836' },
-  { url: 'https://picsum.photos/id/28/4928/3264' },
-  { url: 'https://picsum.photos/id/29/4000/2670' }
+  { url: 'https://picsum.photos/id/0/5000/3333', title: 'عنوان عکس 1' },
+  { url: 'https://picsum.photos/id/1/5000/3333', title: 'عنوان عکس 2' },
+  { url: 'https://picsum.photos/id/2/5000/3333', title: 'عنوان عکس 3' },
+  { url: 'https://picsum.photos/id/3/5000/3333', title: 'عنوان عکس 4' },
+  { url: 'https://picsum.photos/id/4/5000/3333', title: 'عنوان عکس 5' },
+  { url: 'https://picsum.photos/id/5/5000/3334', title: 'عنوان عکس 6' },
+  { url: 'https://picsum.photos/id/6/5000/3333', title: 'عنوان عکس 7' },
+  { url: 'https://picsum.photos/id/7/4728/3168', title: 'عنوان عکس 8' },
+  { url: 'https://picsum.photos/id/8/5000/3333', title: 'عنوان عکس 9' },
+  { url: 'https://picsum.photos/id/9/5000/3269', title: 'عنوان عکس 10' },
+  { url: 'https://picsum.photos/id/10/2500/1667', title: 'عنوان عکس 11' },
+  { url: 'https://picsum.photos/id/11/2500/1667', title: 'عنوان عکس 12' },
+  { url: 'https://picsum.photos/id/12/2500/1667', title: 'عنوان عکس 13' },
+  { url: 'https://picsum.photos/id/13/2500/1667', title: 'عنوان عکس 14' },
+  { url: 'https://picsum.photos/id/14/2500/1667', title: 'عنوان عکس 15' },
+  { url: 'https://picsum.photos/id/15/2500/1667', title: 'عنوان عکس 16' },
+  { url: 'https://picsum.photos/id/16/2500/1667', title: 'عنوان عکس 17' },
+  { url: 'https://picsum.photos/id/17/2500/1667', title: 'عنوان عکس 18' },
+  { url: 'https://picsum.photos/id/18/2500/1667', title: 'عنوان عکس 19' },
+  { url: 'https://picsum.photos/id/19/2500/1667', title: 'عنوان عکس 20' },
+  { url: 'https://picsum.photos/id/20/3670/2462', title: 'عنوان عکس 21' },
+  { url: 'https://picsum.photos/id/21/3008/2008', title: 'عنوان عکس 22' },
+  { url: 'https://picsum.photos/id/22/4434/3729', title: 'عنوان عکس 23' },
+  { url: 'https://picsum.photos/id/23/3887/4899', title: 'عنوان عکس 24' },
+  { url: 'https://picsum.photos/id/24/4855/1803', title: 'عنوان عکس 25' },
+  { url: 'https://picsum.photos/id/25/5000/3333', title: 'عنوان عکس 26' },
+  { url: 'https://picsum.photos/id/26/4209/2769', title: 'عنوان عکس 27' },
+  { url: 'https://picsum.photos/id/27/3264/1836', title: 'عنوان عکس 28' },
+  { url: 'https://picsum.photos/id/28/4928/3264', title: 'عنوان عکس 29' },
+  { url: 'https://picsum.photos/id/29/4000/2670', title: 'عنوان عکس 30' }
 ]
+
+const showLightbox = ref(false)
+const lightboxImage = ref(null)
+
+function openLightbox(image) {
+  lightboxImage.value = image
+  showLightbox.value = true
+}
+
+function closeLightbox() {
+  showLightbox.value = false
+  lightboxImage.value = null
+}
 
 function getGridClass(idx) {
   // 3 columns per row
